d6130ec5d5cf4cc99861ef29bc9e3ab220622b1e
PhilPerk <philipperkins2020@gmail.com>
2021-07-10 13:10:15 -0500

    edits

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 
 
-<<<<<<< HEAD
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 
 
   
-=======
-  
-
->>>>>>> 06ba22a94e96ed7d6b85270e13e9a6b2dadec3a2
             
 function Nav() {
   return (
@@ -35,17 +30,5 @@ function Nav() {
     
   );
 }
-<<<<<<< HEAD
-
-=======
-{
-  <ul className="sidenav"  id=" modile-demo">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Logout</a></li>
-        <li><a href="#">Task</a></li>
-        <li><a href="#">Ranking</a></li>
-  </ul>
-}
->>>>>>> 06ba22a94e96ed7d6b85270e13e9a6b2dadec3a2
 
 export default Nav;
