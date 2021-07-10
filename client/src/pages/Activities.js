0ec6026bc075d5b496fa2217022446be5583b666
Michelle Berta <bertafamily@sbcglobal.net>
2021-07-10 11:07:34 -0500

    installed materialize and reinstalled dependencies

--- client/src/pages/Activities.js
+++ client/src/pages/Activities.js
@@ -1,58 +1,7 @@
-import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import React from "react";
 
 function Activities() {
-  // Setting our component's initial state
-  const [activities, setActivities] = useState([])
-  const [formObject, setFormObject] = useState({})
 
-  // Load all books and store them with setActivities
-  useEffect(() => {
-    loadActivities()
-  }, [])
-
-  // Loads all books and sets them to books
-  function loadActivities() {
-    API.getActivities()
-      .then(res => 
-        setActivities(res.data)
-      )
-      .catch(err => console.log(err));
-  };
-
-  // Deletes a book from the database with a given id, then reloads Activities from the db
-  function deleteActivity(id) {
-    API.deleteBook(id)
-      .then(res => loadActivities())
-      .catch(err => console.log(err));
-  }
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
-
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload Activities from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadActivities())
-        .catch(err => console.log(err));
-    }
-  };
 
     return (
       <Container fluid>
