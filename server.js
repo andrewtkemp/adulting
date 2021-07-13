1fae065e0ec83a43f5445653bbd96c63c44d7c5c
Daniel E Manley <77508379+Dmanley93@users.noreply.github.com>
2021-07-12 21:19:53 -0500

    added some user authentication and the packages needed for it

--- server.js
+++ server.js
@@ -4,19 +4,40 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const expressSession = require('express-session');
+const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require('./models/User');
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/adulting", {useFindAndModify: false });
+
+app.use(expressSession({
+  secret: "this is my super secrety-secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/adulting", {useFindAndModify: false });
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // Start the API server
 app.listen(PORT, function() {
