9ae72332f1d48c1c22d8793110227f83ca7b10f1
Michelle Berta <bertafamily@sbcglobal.net>
2021-07-14 15:41:54 -0500

    created awards and profile components, and adjusted routes on App.js and Home.js accordingly

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,19 @@ import React from "react";
 import Nav from "../components/Nav/index";
 import Activities from "../components/Activities/Activities";
 import Awards from "../components/Awards/Awards";
+import Profile from "../components/Profile/Profile";
 
 function Home() {
-  return <div>
-      <Nav /> 
+  return (
+    <div>
+      <Nav />
       <main className="main-container">
-      <Activities />
-      <Awards />
-
+        <Activities />
+        <Awards />
+        <Profile />
       </main>
-  </div>;
+    </div>
+  );
 }
 
 export default Home;
