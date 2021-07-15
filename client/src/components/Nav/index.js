e7f80e64edf0d3a9ed50372083e89cc227e78bc0
agsauce <76827676+agsauce@users.noreply.github.com>
2021-07-14 19:24:25 -0500

    updated nav bar changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+// home tag from 
 
 
   
@@ -9,18 +10,18 @@ function Nav() {
     
     <nav 
     className="#ff8a65 deep-orange lighten-2, nav-wrapper"  style="padding: 0px 10px;">
-      <a  href="#" className="brand-logo">Aduling App</a>
+      <a  href="#home" className="brand-logo">Aduling App</a>
 
-      <a href="#" className="sidenav-tigger" data-target="mobile-nav">
+      <a href="#home" className="sidenav-tigger" data-target="mobile-nav">
         <i className="material-icons">menu</i>
       </a>
 
         React Reading List
      <ul className= "right hide-on-med-and-down">  
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Logout</a></li>
-        <li><a href="#">Task</a></li>
-        <li><a href="#">Ranking</a></li>
+        <li><a href="#home">Home</a></li>
+        {/* <li><a href="#logout">Logout</a></li> */}
+        
+
 
      </ul>
     </nav>
@@ -29,10 +30,10 @@ function Nav() {
 }
 {
   <ul className="sidenav"  id=" modile-demo">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Logout</a></li>
-        <li><a href="#">Task</a></li>
-        <li><a href="#">Ranking</a></li>
+        <li><a href="#home">home</a></li>
+        {/* <li><a href="#logout">Logout</a></li> */}
+
+  
   </ul>
 }
 
