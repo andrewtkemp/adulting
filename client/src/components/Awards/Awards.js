f5c84d6a82609b612de08405c8eed47f85edd6b3
Michelle Berta <bertafamily@sbcglobal.net>
2021-07-17 14:09:43 -0500

    worked on awards

--- client/src/components/Awards/Awards.js
+++ client/src/components/Awards/Awards.js
@@ -1,18 +1,70 @@
 import React from "react";
 import "./AwardsStyle.css";
+import bronze from "../../assets/bronzeStar.png";
+import silver from "../../assets/silverStar.png";
+import gold from "../../assets/goldStar.png";
+import bronzeSilver from "../../assets/bronze-silverStar.png";
+import bronzeGold from "../../assets/bronze-silver-goldStar.png";
 
 function Awards() {
+  function stars() {
+    const pointsEarned = api.getPoints();
+    if (pointsEarned <= 99) {
+      return <h4>Keep Adulting to Level Up and Earn Awards!</h4>;
+    }
+    if (pointsEarned >= 100) {
+      return (
+        <div>
+          <img src={bronze} alt="bronze star" className="allAwards"></img>
+          <p>Congratulations! You've earned the Bronze Star!</p>
+        </div>
+      );
+    }
+    if (pointsEarned >= 200) {
+      return (
+        <div>
+          <img src={silver} alt="silver star" className="allAwards"></img>
+          <p>Congratulations! You've earned the Silver Star!</p>
+        </div>
+      );
+    }
+    if (pointsEarned >= 300) {
+      return (
+        <div>
+          <img src={gold} alt="gold star" className="allAwards"></img>
+          <p>Congratulations! You've earned the Gold Star!</p>
+        </div>
+      );
+    }
+    if (pointsEarned >= 400) {
+      return (
+        <div>
+          <img src={bronzeSilver} alt="bronzeSilver star" className="allAwards"></img>
+          <p>Congratulations! You've earned the Siler Bronze Star!</p>
+        </div>
+      );
+    }
+    if (pointsEarned >= 500) {
+      return (
+        <div>
+          <img src={bronzeGold} alt="bronzeGold star" className="allAwards"></img>
+          <p>Congratulations! You've earned the Gold Silver Bronze Star!</p>
+        </div>
+      );
+    }
+    return <Awards />;
+  }
+
   return (
     <div className="col s12 m12 l4 awardCard">
       <div className="card">
         <div className="card-content">
           <span className="card-title">Awards</span>
-          <p>This is where we will show the awards that have been achieved.</p>
+
+          <img src={bronze} alt="bronze star" className="materialboxed"></img>
+          <p>Earn points by acting like an adult to level up!</p>
         </div>
-        <div className="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
-        </div>
+        {stars()}
       </div>
     </div>
   );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,26 +1,17 @@
 import React from "react";
-
-
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-
-
-  
-            
+           
 function Nav() {
   return (
     
     <nav 
-    className="#69f0ae green accent-2 nav-wrapper">
-      <a  href="#home" className="brand-logo">Aduling App</a>
+    className="#69f0ae green accent-2 nav-wrapper center-align">
+      <a  href="#home" className="brand-logo">Adulting</a>
 
       <a href="#home" className="sidenav-tigger" data-target="mobile-nav">
         <i className="material-icons">menu</i>
       </a>
-
-        React Reading List
      <ul className= "right hide-on-med-and-down">  
-        <li><a href="#home">Home</a></li>
+        <li><a href="#home"></a></li>
         {/* <li><a href="#logout">Logout</a></li> */}
        
      </ul>
