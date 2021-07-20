b41757c6e229246ce442ec80db63c50c9436ab47
Michelle Berta <bertafamily@sbcglobal.net>
2021-07-20 15:04:26 -0500

    deleted status, fixed errors on nav

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,76 +1,66 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
-import "./Loginstyles.css"
-import { Link } from "react-router-dom"
-import loginPic from "../assets/adulting.png"
-
-
+import "./Loginstyles.css";
+import { Link } from "react-router-dom";
+import loginPic from "../assets/adulting.png";
+import Nav from "../components/Nav";
 
 function Login(props) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-     console.log(email,password)
-        axios
-            .post(
-                "api/user/login",
-                {
-                    email, password
-                },
-                { withCredentials: true }
-            )
-            .then(response => {
-                console.log(response);
-                window.location.replace('/home');
-            })
-            .catch(error => {
-                console.log("login error", error);
-            });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+    axios
+      .post(
+        "api/user/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log(response);
+        window.location.replace("/home");
+      })
+      .catch((error) => {
+        console.log("login error", error);
+      });
+  };
 
-
-    }
-
-
-
-    return (
-        <div className="row center-align " >
-            <div className="col s6 pic">
-                <img src={loginPic} className="materialboxed" width="325" />
-
-            </div>
-
-
-            <div className="col s6 center">
-                <div className="input-field ">
-                    <i className="material-icons prefix">account_circle</i>
-                    <input name="email" placeholder="Email" onChange={e => setEmail(e.target.value)} id="icon_prefix" type="text" className="validate"></input>
-                </div>
-                <div className="input-field ">
-                    <i className="material-icons prefix">security</i>
-                    <input name="password" placeholder="Password" onChange={e => setPassword(e.target.value)} id="icon_prefix" type="text" className="validate"></input>
-                </div>
-                <button className="submit" onClick={handleSubmit}>Submit </button>
-               
-            </div>
-            
-            
-            <div>
-                <Link className="submit" to="/signup">Create an Account</Link>
-            </div>
+  return (
+    <div>
+      <Nav />
+      <div className="row center-align ">
+        <div className="col s6 pic">
+          <img src={loginPic} className="materialboxed" width="325" />
         </div>
 
+        <div className="col s6 center">
+          <div className="input-field ">
+            <i className="material-icons prefix">account_circle</i>
+            <input name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} id="icon_prefix" type="text" className="validate"></input>
+          </div>
+          <div className="input-field ">
+            <i className="material-icons prefix">security</i>
+            <input name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} id="icon_prefix" type="text" className="validate"></input>
+          </div>
+          <button className="submit" onClick={handleSubmit}>
+            Submit{" "}
+          </button>
+        </div>
 
-
-
-
-
-    );
+        <div>
+          <Link className="submit" to="/signup">
+            Create an Account
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
 
 export default Login;
