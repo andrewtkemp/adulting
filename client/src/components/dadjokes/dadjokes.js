7ec6ee6ad5bcc26b35d646f8841be71f1b29e380
agsauce <76827676+agsauce@users.noreply.github.com>
2021-07-21 21:19:58 -0500

    dad jokes api and home modal

--- client/src/components/dadjokes/dadjokes.js
+++ client/src/components/dadjokes/dadjokes.js
@@ -1,32 +1,47 @@
-//  start of jokes 
-function dadjokes() {
-    fetch("https://dad-jokes.p.rapidapi.com/random/joke/png", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "dad-jokes.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-  }
-    then(response => response.json())
-  .then(data => {
-      console.log(data);
-  
-      // populating Id jokes with inside conslog 
-      $("#dadjokes").text(data.value);
-  })
-  .catch(error => {
-      console.log(error);
-  })
-  
-  
-  displayBookmarks();
-  
-  // calling display jokes
-  dadjokes();
+import React from "react";
+import axios from "axios";
+import { useState,useEffect } from "react";
 
+
+
+function Dadjokes(props) {
+const[joke, setjokes]=useState()
+  //  start of jokes 
+  function dadjokes() {
+    var options = {
+      method: 'GET',
+      url: 'https://dad-jokes.p.rapidapi.com/random/joke/png',
+      headers: {
+        'x-rapidapi-key': 'e8033ca9a0msh25b010323f351bfp10467ejsn0155295ca703',
+        'x-rapidapi-host': 'dad-jokes.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setjokes(response.data.body.setup.punchline)
+       console.log(Dadjokes) 
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+  useEffect(() => {
+    // Update the document title using the browser API
+   dadjokes()
+  },[]);
+  return (
+    <>
+      <a class="waves-effect waves-light btn modal-trigger" href="home">Modal</a>
+
+      <div id="modal1" class="modal modal-fixed-footer">
+        <div class="modal-content">
+          <h4>dadjokes</h4>
+          <p></p>
+        </div>
+        <div class="modal-footer">
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
+      </div>
+    </>)
+}
+export default Dadjokes;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "../components/Nav/index";
 import Activities from "../components/Activities/Activities";
 import Awards from "../components/Awards/Awards";
+import Dadjokes from "../components/dadjokes/dadjokes";
 
 function Home() {
   return (
@@ -9,7 +10,7 @@ function Home() {
       <Nav />
       <main className="main-container" id="home">
         <div className="row">
-          {/* "dadjoke modol" */}
+          <Dadjokes/>
           <Activities />
           <Awards />
         </div>
